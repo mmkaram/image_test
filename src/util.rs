f9b4d66991bb10cd4888e1fc54a4aabd5b9ca179
mmkaram <mahdykaram@outlook.com>
2024-11-10 16:24:25 -0500

    get contours

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,15 @@
+use opencv::{core::{Point, Vector}, imgproc};
+
 pub fn find_likely_balls(frame: &opencv::prelude::Mat) {
 
-    let mut contours = opencv::types::VectorOfPoint::new();
-    let mut contours_two = core::Vector<core::Point>;
+    let mut contours= Vector::<Point>::new();
 
-    let _ = opencv::imgproc::find_contours(
+    let _ = imgproc::find_contours(
         frame,
         &mut contours,
-        opencv::imgproc::RETR_EXTERNAL,
-        opencv::imgproc::CHAIN_APPROX_SIMPLE,
-        opencv::core::Point::new(0,0)
+        imgproc::RETR_EXTERNAL,
+        imgproc::CHAIN_APPROX_SIMPLE,
+        Point::new(0,0)
     );
 
 }
