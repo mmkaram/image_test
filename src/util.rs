d068e3854d075911a712c7337613567c641ad088
mmkaram <mahdykaram@outlook.com>
2024-11-10 16:07:50 -0500

    find contours

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,13 @@
-use opencv::{prelude::Mat, videoio};
+pub fn find_likely_balls(frame: &opencv::prelude::Mat) {
 
-pub fn find_likely_balls(frame: &opencv::prelude::Mat) -> Mat {
-    return frame.clone();
+    let mut contours = opencv::types::VectorOfPoint::new();
+
+    let _ = opencv::imgproc::find_contours(
+        frame,
+        &mut contours,
+        opencv::imgproc::RETR_EXTERNAL,
+        opencv::imgproc::CHAIN_APPROX_SIMPLE,
+        opencv::core::Point::new(0,0)
+    );
+
 }
